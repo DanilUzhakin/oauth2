01bc75b48762182a26dfe87b8157d0935cb4a888
DanilUzhakin <57945389+DanilUzhakin@users.noreply.github.com>
2020-05-27 15:16:33 +0500

    Create userservice.java

---
+++ src/main/java/com/example/demo/services/userservice.java
@@ -0,0 +1,47 @@
+package com.example.demo.services;
+
+import com.example.demo.entities.User;
+import com.example.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService
+{
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User userFindByUsername = userRepo.findByUsername(username);
+        User userFindByName = userRepo.findByName(username);
+        User userFindByGoogleUsername = userRepo.findByGoogleUsername(username);
+        User userFindByGoogleName = userRepo.findByGoogleName(username);
+
+        if(userFindByUsername != null)
+        {
+            return userFindByUsername;
+        }
+
+        if(userFindByName != null)
+        {
+            return userFindByName;
+        }
+
+        if(userFindByGoogleUsername != null)
+        {
+            return userFindByGoogleUsername;
+        }
+
+        if(userFindByGoogleName != null)
+        {
+            return userFindByGoogleName;
+        }
+
+        return null;
+    }
+}
